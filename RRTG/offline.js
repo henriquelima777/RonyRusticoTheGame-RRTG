f3cca5c2bf18205ce6626326204ffcdfe23be6ac
Henrique de Lima <76058861+henriquelima777@users.noreply.github.com>
2024-06-06 08:37:42 -0300

    Add files via upload

---
+++ RRTG/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1717673649,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/rony-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/copalibertadores-sheet0.png",
+		"images/copadobr-sheet0.png",
+		"images/paulistao-sheet0.png",
+		"images/ohomemdopacto-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
